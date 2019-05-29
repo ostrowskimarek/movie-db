f28f7be27ac1bc1fabaf83d3588c4a52c6bfa56a
ostrowskimarek <ostrowskimarek1@gmail.com>
2019-05-29 19:11:52 +0200

    Updates

---
+++ precache-manifest.c5047aec050256cc5e2611515a90e3e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef96be04f6c56b408a4c8eed838e5b7",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "b96dfab7ee31ccba746e",
+    "url": "/movie-db/static/css/main.5ce2d30e.chunk.css"
+  },
+  {
+    "revision": "17668d01afca0d313a68",
+    "url": "/movie-db/static/js/2.1ab6adf9.chunk.js"
+  },
+  {
+    "revision": "b96dfab7ee31ccba746e",
+    "url": "/movie-db/static/js/main.5652fcfc.chunk.js"
+  },
+  {
+    "revision": "6bb80631b382c7965b33",
+    "url": "/movie-db/static/js/runtime~main.5114c00d.js"
+  }
+]);
